0a0081eb1fccd9eb26f04102090ee39a1155743a
Joao Bosco <jboscos.carvalho@gmail.com>
2023-04-13 23:37:33 -0300

    fix: routes post and get /participants

--- src/app.js
+++ src/app.js
@@ -61,7 +61,7 @@ app.post("/participants", async (req, res) => {
       time: dayjs().format("HH:mm:ss"),
     });
 
-    res.sendStatus(200);
+    res.sendStatus(201);
   } catch (err) {
     res.status(500).send(err.message);
   }
